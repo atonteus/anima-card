4ef4ae2b11842c91cc1cfe0725616a246adb522e
Antonio Mateus <uidesign@fractalbases.com>
2022-01-19 23:40:33 -0300

    body

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+
+const title = document.querySelector(".title");
+
+const purchase = document.querySelector(".purchase");
+const sizes = document.querySelector(".sizes");
+
